63523ca767b5942c083ec91dad3664ad86cd8255
Johannes Bornhold <johannes@bornhold.name>
2017-10-30 14:58:33 +0100

    Update pip2nix

--- default.nix
+++ default.nix
@@ -9,8 +9,8 @@ let
   pythonPackages = getAttr _pythonPackages pkgs;
 
   pip2nix-src = pkgs.fetchzip {
-    url = https://github.com/johbo/pip2nix/archive/5950e2c74f4a56e8b3a0062593a1892d4998553a.tar.gz;
-    sha256 = "1aaahsxcirlsdx6p42k0dhg459av3d6chr03d8s8gcjxkfwc3fxm";
+    url = https://github.com/johbo/pip2nix/archive/cf148dc2b915e7179c572d2c2317aef3dc3c18f2.tar.gz;
+    sha256 = "0izzppljfdsnn2l7l6z08gw5gx7y56nkd5zkm079v7gkzhszgskd";
   };
 
   pip2nix = import pip2nix-src {
